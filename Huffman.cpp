8fee9c0cdaff78097fbabad4f6b47ac30715a77f
simjeongho012@gmail.com <simjeongho012@gmail.com>
2022-04-28 15:06:51 +0900

    허프만 알고리즘 구현

--- Huffman.cpp
+++ Huffman.cpp
@@ -4,7 +4,7 @@
 #include <queue>
 #include <fstream>
 #include <string>
-
+#include <unordered_map>
 using namespace std;
 struct huffmanNode {
     char a;
@@ -12,62 +12,108 @@ struct huffmanNode {
     huffmanNode* left;
     huffmanNode* right;
 };
-huffmanNode ascii[127];
+
 struct cmp {
-    bool operator()(huffmanNode a, huffmanNode b) {
-        return a.fre > b.fre;
+    bool operator()(huffmanNode *a, huffmanNode *b) {
+        return a->fre > b->fre;
     }
 };
-priority_queue<huffmanNode, vector<huffmanNode>, cmp> pq;
-int main() {
 
-    ifstream file("test.txt");
-    string str;
-    while (getline(file, str)) {
-        cout << str << "\n";
-        for (char a : str) {
-            //ascii[a - 'a'] ++;
-            ascii[a].fre ++;
-            ascii[a].a = a;
+
+class HuffmanTree {
+private: 
+    huffmanNode* root = nullptr;
+    unordered_map <char, int> ascii;
+    unordered_map<char, string> code;
+    priority_queue<huffmanNode*, vector<huffmanNode*>, cmp> pq;
+    string text;
+public:
+    ~HuffmanTree() {
+        releaseTree(root);
+        root = nullptr;
+        while (!pq.empty()) pq.pop();
+    }
+
+public:
+    void Start() {
+        ifstream file("test.txt");
+        while (getline(file, text)) {
+            cout << text << "\n";
+            for (char a : text) {
+                //ascii[a - 'a'] ++;
+                /*ascii[a].fre++;
+                ascii[a].a = a;*/
+                ++ascii[a];
+            }
+        }
+        //pq에 삽입
+        for (const auto iter : ascii) {
+            huffmanNode* newhuffman = new huffmanNode;
+            newhuffman->left = nullptr;
+            newhuffman->right = nullptr;
+            newhuffman->a = iter.first;
+            newhuffman->fre = iter.second;
+            pq.push(newhuffman);
+        }
+        MakeTree();
+        string tmp = "";
+        FindTree(root, tmp);
+    }
+    void showCode() {
+        for (auto it = code.begin(); it != code.end(); ++it)
+        {
+            cout << it->first << " : " << it->second << "\n";
         }
     }
-    for (int i = 0; i < 127; i++) {
-        cout << ascii[i].fre << " ";
-    }
-    cout << "\n";
-    for (int i = 0; i < 127; i++) {
-        pq.push(ascii[i]);
-    }
-    cout << pq.size() << "\n";
-    /*cout << pq.top() << " ";
-    pq.pop();
-    cout << pq.top() << " ";
-    pq.pop();
-    cout << pq.top() << " ";
-    pq.pop();*/
-    while (pq.top().fre == 0) {
-        pq.pop();
-    }
-    while (!pq.empty()) {
-        cout << pq.top().a << " " << pq.top().fre << " ";
-        pq.pop();
-    }
-    for (int i = 1; i < pq.size(); i++) {
-        huffmanNode p = pq.top();
-        pq.pop();
-        huffmanNode q = pq.top();
-        pq.pop();
-        huffmanNode r;
-        r.left = &p;
-        r.right = &q;
-        r.fre = p.fre + q.fre;
-        pq.push(r);
+private:
+    void MakeTree() {
+        int lim = pq.size() - 1;
+        for (int i = 0; i < lim; i++) {
+            huffmanNode* p = pq.top();
+            pq.pop();
+            huffmanNode* q = pq.top();
+            pq.pop();
+            huffmanNode* r = new huffmanNode;
+            r->left = p;
+            r->right = q;
+            r->fre = p->fre + q->fre;
+            r->a = 0;
+            pq.push(r);
+            cout << "Make Tree시작" << r->left->a << r->left->fre << " " << r->right->a << r->right->fre << "\n";
+            cout << "r->a : " << r->a << "r=>fre : " << r->fre << "\n";
+        }
+        root = pq.top();
+        cout << "top is" << root->a << " " << root->fre << "\n";
     }
 
-    /*while (!pq.empty()) {
-        cout << pq.top().a << " " << pq.top().fre << " " ;
-        pq.pop();
-   }*/
+    void FindTree(huffmanNode* p, string str) {
+        if (p == nullptr) return;
+        FindTree(p->left, str + '0');
+        FindTree(p->right, str + '1');
+        if (p->a != 0)
+        {
+            cout << str << "\n";
+            //code map에 집어 넣기 
+            code[p->a] = str;
+        }
+    }
 
+    void releaseTree(huffmanNode* p)
+    {
+        if (p == nullptr) return;
+        releaseTree(p->left);
+        releaseTree(p->right);
+        delete p;
+        p = nullptr;
+    }
+
+    
+
+};
+int main() {
+
+    HuffmanTree T;
+    T.Start();
+    T.showCode();
     return 0;
 }
