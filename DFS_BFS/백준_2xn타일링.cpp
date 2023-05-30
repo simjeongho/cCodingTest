7c61e6ec9ff1d6c679c670d21d02c15cab4fd75e
simjeongho <simjeongho012@gmail.com>
2023-05-30 14:11:12 +0900

    dp+ dfs

---
+++ DFS_BFS/백준_2xn타일링.cpp
@@ -0,0 +1,19 @@
+#pragma warning (disable : 4996)
+#include <cstdio>
+#define MAX 1002
+using namespace std;
+int n;
+int dp[MAX];
+
+int solution(int num) {
+	if (num == 1) return 1;
+	if (num == 2) return 2;
+	if (dp[num] != 0)return dp[num];
+	return dp[num] = (solution(num - 1) +solution(num - 2)) % 10007;
+}
+int main() {
+	scanf("%d", &n);
+
+	printf("%d", solution(n));
+	return 0;
+}
